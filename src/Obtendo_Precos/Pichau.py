a15f5ca57ad3b65e21f5e30bce8d1d124d66355e
danielcm01 <danielmattar2001@gmail.com>
2021-05-26 12:59:02 -0300

    Refactored: adapted to new html on Pichau

--- src/Obtendo_Precos/Pichau.py
+++ src/Obtendo_Precos/Pichau.py
@@ -17,32 +17,50 @@ def getName(array_text):
                 nome = array_text[x+1]
                 return nome
 
-def getPrice(array_text):
-    first = True
-    x = 0
-    text = ""
-    while x < len(array_text) and first == True:
-        if array_text[x] == "vista":
-            first = False
-            text = array_text[x+1]
-        x = x + 1
-    if first == False:
-        array = [None]*len(text)
-        for x in range(len(text)):
-            array[x] = text[x]
-        if check_array(array,".") == True:
+# def getPrice(array_text):
+#     first = True
+#     x = 0
+#     text = ""
+#     while x < len(array_text) and first == True:
+#         if array_text[x] == "vista":
+#             first = False
+#             text = array_text[x+1]
+#         x = x + 1
+#     if first == False:
+#         array = [None]*len(text)
+#         for x in range(len(text)):
+#             array[x] = text[x]
+#         if check_array(array,".") == True:
+#             array.remove(".")
+#         array.remove("R")
+#         array.remove("$")
+#         for x in range(len(array)):
+#             if array[x] == ",":
+#                 posicao = x
+#             else:
+#                 array[x] = float(array[x])
+#         preco = number_array_to_value(array,posicao)
+#         return preco
+#     else:
+#         return "Produto indisponivel"
+
+def getPrice(preco):
+    array = []
+    for x in range(len(preco)):
+        array.append(preco[x])
+    array.remove("R")
+    array.remove("$")
+    if check_array(array,".") == True:
             array.remove(".")
-        array.remove("R")
-        array.remove("$")
-        for x in range(len(array)):
-            if array[x] == ",":
-                posicao = x
-            else:
-                array[x] = float(array[x])
-        preco = number_array_to_value(array,posicao)
-        return preco
-    else:
-        return "Produto indisponivel"
+    for x in range(len(array)):
+        if array[x] == ',':
+            posicao = x
+        else:
+            array[x] = float(array[x])
+    value = number_array_to_value(array,posicao)
+    return value
+
+
 def getMemory(array_text):
     first = True
     x = 0
@@ -66,56 +84,98 @@ def getMemory(array_text):
                     z = z + 1 
         x = x + 1
 
+def find_next(caracteristica,attrs):
+    secao = caracteristica.find_next(attrs)
+    return secao
+
 def makeDictionary(page):
     URL = "https://www.pichau.com.br/hardware/placa-de-video?p="+page+"&product_list_limit=48&product_list_order=price_desc"
     headers = {'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.192 Safari/537.36"}
     site = requests.get(URL, headers=headers)
     soup = BeautifulSoup(site.content,'html.parser')  
+    z = len(PICHAU)
+    position = []*z
     x = len(PICHAU)
-    for tag in soup.find_all('div',class_="product details product-item-details"):
-        heading = tag.text
+    html = soup.h2
+    initlen = len(PICHAU)
+    while (z < (48 + initlen)) and (html != None):
+        heading = html.text
         heading_array = heading.split()
-        if (getName(heading_array) != None) or (getPrice(heading_array) != "Produto indisponivel"): 
+        nome = getName(heading_array)
+        memoria = getMemory(heading_array)
+        position.append(0)
+        if nome != None:
+            position[z] = 1 
             PICHAU[x] = x
             y = {}
-            y['nome'] = getName(heading_array)
-            y['memoria'] = getMemory(heading_array)
-            y['preco'] = getPrice(heading_array)
+            y['nome'] = nome
+            y['memoria'] = memoria
+            y['preco'] = "Produto indisponivel"
             PICHAU[x] = y
             x = x + 1
+        z = z+ 1
+        html = find_next(html,"h2")
+    x = 0
+    body = soup.body
+    div1 = body.contents[2]
+    main = div1.contents[1]
+    div2 = main.contents[1]
+    div3 = div2.contents[0]
+    div4 = div3.contents[0]
+    div5 = div4.contents[2]
+    y = initlen
+    for x in range(z+y):
+        print("\nx: ",x)
+        print("\nlen(position)",len(position))
+        if position[x] == 1:
+            div6 = div5.contents[x]
+            a1 = div6.contents[0]
+            div7 = a1.contents[0]
+            div8 = div7.contents[1]
+            div9 = div8.contents[1]
+            if div9.get_text() != "Esgotado":
+                div10 = div9.contents[0]
+                div11 = div10.contents[0]
+                div12 = div11.contents[1]
+                preco = div12.get_text()
+                valor = getPrice(preco)
+                PICHAU[y]['preco'] = valor
+            else:
+                PICHAU[y]['preco'] = "Produto indisponivel"
+            y = y + 1
             
-def find_next(soup,page):
-    for x in range(6):
-        print("page: antes: ",page)
-        print("page.find_next_sibling(): ",page.find_next_sibling())
-        # print("page: depois: ",page)
-    page = page.get_text()
-    print("page: ",page)
-    return page
-
 def getLastPage():
     URL = "https://www.pichau.com.br/hardware/placa-de-video?p=1&product_list_limit=48&product_list_order=price_desc&sort=price-desc"
     headers = {'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.192 Safari/537.36"}
     site = requests.get(URL, headers=headers)
     soup = BeautifulSoup(site.content,'html.parser') 
-    todos = soup.find('ul',class_="MuiPagination-ul")
-    print(todos)
-    page = todos.contents[0]
-    page = find_next(soup,page)
+    body = soup.body
+    div1 = body.contents[2]
+    main = div1.contents[1]
+    div2 = main.contents[1]
+    div3 = div2.contents[0]
+    div4 = div3.contents[0]
+    nav1 = div4.contents[3]
+    ul1 = nav1.contents[0]
+    li1 = ul1.contents[7]
+    button1 = li1.contents[0]
+    page = button1.get_text()
+    page = int(page)
     return page
 
-    # find_next(7)
 def attDictionary():
-    # page = getLastPage()
-    page = 3
+    page = getLastPage()
     indisponivel = False
+
     print("atualizando dicionario...")
-    for x in range(1,page+1):
+    for x in range(1,page):
         if indisponivel == False:
             string = str(x)
             makeDictionary(string)
-        if PICHAU[len(PICHAU)]['preco'] == "Produto indisponivel":
+            print("\nlen(PICHAU): ",len(PICHAU))
+        if PICHAU[len(PICHAU)-1]['preco'] == "Produto indisponivel":
             indisponivel = True
 attDictionary()
+
 for item in PICHAU:
     print(PICHAU[item])
